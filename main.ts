45223dcff5126f86674f033ceffd26199594e8e6
mobtrucky <128086337+mobtrucky@users.noreply.github.com>
2023-03-16 16:33:06 -0300

    iniciando programaçao para interligar dois bbc microbit para ter 2 sensores

--- main.ts
+++ main.ts
@@ -1,22 +1,6 @@
 basic.forever(function () {
-    // Branco e branco
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 254 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 254 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 254)) {
-        servos.P1.run(50)
-        servos.P2.run(-50)
-    }
-    // Branco e Preto
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 254 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 254 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 254)) {
-        servos.P1.run(50)
-        servos.P2.run(-50)
-    }
-    // Preto e Branco
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 254 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 254 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 254)) {
-        servos.P1.run(50)
-        servos.P2.run(-50)
-    }
-    // Preto e Preto
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 254 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 254 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 254)) {
-        servos.P1.run(50)
-        servos.P2.run(-50)
+    // Vendo cinza
+    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 138 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) < 180 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 138 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 180 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 138 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 180 && false)))))) {
+        basic.showNumber(1)
     }
 })
