16c8ff2daf952e7dc75ad8140f8973f878744eee
mobtrucky <128086337+mobtrucky@users.noreply.github.com>
2023-03-23 17:38:36 -0300

    jkhjkbhjh

--- main.ts
+++ main.ts
@@ -1,44 +1,14 @@
 radio.onReceivedNumber(function (receivedNumber) {
     // Branco e branco
-    if (receivedNumber == 0 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 254 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 254 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 254))) {
-        servos.P1.run(50)
-        servos.P2.run(-50)
-    }
-    // Preto e preto
-    if (receivedNumber == 1 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) == 0 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) == 0 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) == 0))) {
-        servos.P1.run(50)
-        servos.P2.run(50)
-    }
-    // Branco e preto
-    if (receivedNumber == 0 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) == 0 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) == 0 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) == 0))) {
-        servos.P1.run(-50)
-        servos.P2.run(-50)
-    }
-    // Preto e branco
-    if (receivedNumber == 1 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 254 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 254 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 254))) {
-        servos.P1.run(50)
-        servos.P2.run(-50)
+    if (receivedNumber == 0 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 230 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 230 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 230))) {
+        servos.P1.run(60)
+        servos.P2.run(-60)
+    } else {
+        servos.P1.run(-60)
+        servos.P2.run(-60)
     }
 })
+basic.showNumber(3)
 basic.forever(function () {
-    // Branco e branco
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 254 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 254 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 254)) {
-        servos.P1.run(50)
-        servos.P2.run(-50)
-    }
-    // Branco e Preto
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 254 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 254 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 254)) {
-        servos.P1.run(50)
-        servos.P2.run(-50)
-    }
-    // Preto e Branco
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 254 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 254 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 254)) {
-        servos.P1.run(50)
-        servos.P2.run(-50)
-    }
-    // Preto e Preto
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 254 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 254 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 254)) {
-        servos.P1.run(50)
-        servos.P2.run(-50)
-    }
+    radio.setGroup(5)
 })
