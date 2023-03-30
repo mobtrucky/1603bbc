074b3d0baca82491a47c8c91750b0cbb1b8eb3ce
mobtrucky <128086337+mobtrucky@users.noreply.github.com>
2023-03-30 16:10:07 -0300

    Definição das cores vistas pelo bbc

--- main.ts
+++ main.ts
@@ -1,44 +1,26 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    // Branco e branco
-    if (receivedNumber == 0 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 254 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 254 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 254))) {
-        servos.P1.run(50)
-        servos.P2.run(-50)
-    }
-    // Preto e preto
-    if (receivedNumber == 1 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) == 0 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) == 0 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) == 0))) {
-        servos.P1.run(50)
-        servos.P2.run(50)
-    }
-    // Branco e preto
-    if (receivedNumber == 0 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) == 0 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) == 0 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) == 0))) {
-        servos.P1.run(-50)
-        servos.P2.run(-50)
-    }
-    // Preto e branco
-    if (receivedNumber == 1 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 254 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 254 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 254))) {
-        servos.P1.run(50)
-        servos.P2.run(-50)
-    }
-})
 basic.forever(function () {
-    // Branco e branco
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 254 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 254 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 254)) {
-        servos.P1.run(50)
-        servos.P2.run(-50)
+    // Vendo branco
+    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 230 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 230 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 230)) {
+        servos.P1.run(100)
+        servos.P2.run(-100)
+    } else {
+        servos.P2.stop()
+        servos.P1.stop()
     }
-    // Branco e Preto
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 254 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 254 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 254)) {
-        servos.P1.run(50)
-        servos.P2.run(-50)
+    // Vendo azul/ preto
+    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 20 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 20 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 70)) {
+        servos.P1.run(-100)
+        servos.P2.run(100)
+    } else {
+        servos.P2.stop()
+        servos.P1.stop()
     }
-    // Preto e Branco
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 254 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 254 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 254)) {
-        servos.P1.run(50)
-        servos.P2.run(-50)
-    }
-    // Preto e Preto
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 254 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 254 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 254)) {
-        servos.P1.run(50)
-        servos.P2.run(-50)
+    // Vendo verde
+    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 20 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 60 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) < 20)) {
+        servos.P1.run(100)
+        servos.P2.run(-100)
+    } else {
+        servos.P2.stop()
+        servos.P1.stop()
     }
 })
