2f08f9bb568c78135ba32331411069410b465f2e
mobtrucky <128086337+mobtrucky@users.noreply.github.com>
2023-03-30 17:22:03 -0300

    Configurações adicionais de velocidade

--- main.ts
+++ main.ts
@@ -1,26 +1,26 @@
 radio.onReceivedNumber(function (receivedNumber) {
     // Branco e branco
     if (receivedNumber == 0 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 230 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 230 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 230))) {
-        servos.P1.run(100)
-        servos.P2.run(-100)
+        servos.P1.run(50)
+        servos.P2.run(-50)
     }
     // Preto e preto
     if (receivedNumber == 1 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 20 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 20 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 70))) {
-        servos.P1.run(100)
-        servos.P2.run(-100)
+        servos.P1.run(50)
+        servos.P2.run(-50)
     }
     // Branco e preto
     if (receivedNumber == 0 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 20 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 20 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 70))) {
-        servos.P1.run(-50)
-        servos.P2.run(-50)
+        servos.P1.run(50)
+        servos.P2.run(50)
     }
     // Preto e branco
     if (receivedNumber == 1 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 230 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 230 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 230))) {
-        servos.P1.run(50)
-        servos.P2.run(50)
+        servos.P1.run(-50)
+        servos.P2.run(-50)
     }
 })
 basic.showNumber(3)
 basic.forever(function () {
-    radio.setGroup(42)
+    radio.setGroup(5)
 })
