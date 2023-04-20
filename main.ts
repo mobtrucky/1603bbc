98aed0629c85029104a1cac4682de4f5b288f88c
Mobtrucky <128086337+mobtrucky@users.noreply.github.com>
2023-04-20 18:01:05 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
 enum RadioMessage {
     message1 = 49434,
     branco = 12276,
-    preto = 24635
+    preto = 24635,
+    verde = 14709
 }
+radio.onReceivedMessage(RadioMessage.verde, function () {
+    // Preto e verde
+    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 170 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 70 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) < 170)) {
+        // Preto e preto
+        if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 70 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 70 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 70)) {
+            servos.P2.stop()
+            servos.P1.stop()
+        }
+    }
+})
 radio.onReceivedMessage(RadioMessage.branco, function () {
     // Branco e branco
     if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 30 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 30 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 30)) {
