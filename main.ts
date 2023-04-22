a4f0a71d999247be23a0d3c458c07dd8fc4c3cf5
Mobtrucky <128086337+mobtrucky@users.noreply.github.com>
2023-04-21 21:43:24 -0300

    teste de 3 cores no 4

--- main.ts
+++ main.ts
@@ -9,16 +9,17 @@ radio.setGroup(5)
 basic.forever(function () {
     // Vendo preto
     if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 25 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 25 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 67)) {
+        basic.showString("p")
         radio.sendMessage(RadioMessage.preto)
     }
     // vendo branco
     if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 30 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 30 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 30)) {
+        basic.showString("b")
         radio.sendMessage(RadioMessage.branco)
     }
     // vendo verde
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 170 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 70 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) < 170)) {
-        if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 70 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 70 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 70)) {
-            radio.sendMessage(RadioMessage.verde)
-        }
+    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 39 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 250 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) < 39)) {
+        basic.showString("v")
+        radio.sendMessage(RadioMessage.verde)
     }
 })
