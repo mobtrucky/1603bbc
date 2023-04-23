d45c857abfb0c22f6fb82c34beeaef2eb2053c3d
Mobtrucky <128086337+mobtrucky@users.noreply.github.com>
2023-04-22 22:26:51 -0300

    mais alterações. Provavelmente inúteis.

--- main.ts
+++ main.ts
@@ -5,30 +5,49 @@ enum RadioMessage {
     verde = 14709
 }
 radio.onReceivedMessage(RadioMessage.verde, function () {
-    // Preto e verde
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 39 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 250 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) < 39)) {
-        basic.showString("v")
-    }
+	
 })
 radio.onReceivedMessage(RadioMessage.branco, function () {
+    // Branco e preto
+    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 25 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 25 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) < 25)) {
+        servos.P1.run(20)
+        servos.P2.run(-20)
+        // 1 branco preto
+        basic.showString("1")
+        basic.clearScreen()
+    }
     // Branco e branco
     if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 30 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 30 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 30)) {
-        basic.showString("bb")
-    }
-    // Branco e preto
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 40 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 40 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 50)) {
-        basic.showString("bp")
+        servos.P1.run(20)
+        servos.P2.run(-20)
+        basic.showString("b")
+        basic.clearScreen()
     }
 })
 radio.onReceivedMessage(RadioMessage.preto, function () {
     // Preto e preto
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 40 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 40 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 50)) {
-        basic.showString("pp")
+    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 25 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 25 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 67)) {
+        servos.P1.run(20)
+        servos.P2.run(-20)
     }
     // Preto e branco
     if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 30 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 30 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 30)) {
-        basic.showString("pb")
+        servos.P1.run(-20)
+        servos.P2.run(-20)
     }
 })
 radio.setGroup(5)
 basic.showNumber(3)
+basic.forever(function () {
+    // Branco e preto
+    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 25 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 25 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 150)) {
+        // 1 branco preto
+        basic.showString("p")
+        basic.clearScreen()
+    }
+    // Preto e verde
+    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 39 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 250 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) < 39)) {
+        basic.showString("v")
+        basic.clearScreen()
+    }
+})
