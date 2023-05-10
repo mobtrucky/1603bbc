a03e46a663fe37920c83bda226b540d5fedc737c
Mobtrucky <128086337+mobtrucky@users.noreply.github.com>
2023-05-10 18:01:21 -0300

    alterações simples nas velocidades

--- main.ts
+++ main.ts
@@ -14,16 +14,16 @@ radio.onReceivedMessage(RadioMessage.verde, function () {
 radio.onReceivedMessage(RadioMessage.branco, function () {
     // Branco e preto
     if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 36 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 85 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) == 255)) {
-        servos.P1.run(-20)
-        servos.P2.run(-20)
+        servos.P1.run(40)
+        servos.P2.run(40)
         // 1 branco preto
         basic.showString("1")
         basic.clearScreen()
     }
     // Branco e branco
     if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) >= 36 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) >= 85 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) == 255)) {
-        servos.P1.run(20)
-        servos.P2.run(-20)
+        servos.P1.run(15)
+        servos.P2.run(-15)
         basic.showString("b")
         basic.clearScreen()
     }
@@ -31,15 +31,15 @@ radio.onReceivedMessage(RadioMessage.branco, function () {
 radio.onReceivedMessage(RadioMessage.preto, function () {
     // Preto e preto
     if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 36 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 85 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) == 255)) {
-        servos.P1.run(20)
-        servos.P2.run(-20)
+        servos.P1.run(15)
+        servos.P2.run(-15)
         basic.showString("p")
         basic.clearScreen()
     }
     // Preto e branco
     if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) >= 36 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) >= 85 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) == 255)) {
-        servos.P1.run(-20)
-        servos.P2.run(-20)
+        servos.P1.run(-40)
+        servos.P2.run(-40)
         // 2 preto branco
         basic.showString("2")
         basic.clearScreen()
